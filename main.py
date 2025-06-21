47220598a594cf18c5bad14c116bdded1a708fac
whitcriz <u.gulsumkarabudak@gmail.com>
2025-06-21 16:51:13 +0300

    fill screen and draw player triangle

--- main.py
+++ main.py
@@ -3,19 +3,25 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
+        player.draw(screen)
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        delta_time = clock.tick(60)
+        dt = delta_time / 1000
 
 if __name__ == "__main__":
     main()
